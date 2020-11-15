be92a61e8f72ead516ef5cdbaf4a20f049f715ba
vnaang <anglvd84@gmail.com>
2020-11-15 15:46:53 +0700

    git commit

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,20 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+         UserService userService = new UserServiceImpl();
+
+         String testName = "Ivan";
+         String testLastName = "Ivanov";
+         byte testAge = 5;
+
+         userService.createUsersTable();
+         userService.saveUser(testName, testLastName, testAge);
+         System.out.println(userService.getAllUsers());
+         userService.cleanUsersTable();
+         userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,24 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String DRIVER = "com.mysql.jdbc.Driver";
+    private static final String URL = "jdbc:mysql://localhost/foo?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+    private static final String LOGIN = "root";
+    private static final String PASSWORD = "4891";
+
+    public Connection getConnection(){
+        Connection connection = null ;
+        try {
+            Class.forName(DRIVER);
+            connection = DriverManager.getConnection(URL, LOGIN, PASSWORD);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
 }
